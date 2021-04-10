febbedd6bd82ff5ecc95cdb81718feb3c0b1eb98
Mayank Raj <mayank42293@gmail.com>
2021-04-10 16:59:42 +0530

    segment tree

---
+++ TEMPLATES/KmpAlgorithm.cpp
@@ -0,0 +1,104 @@
+//              .-""""-.
+//                        / j      \
+//                       :.d;       ;
+//                       $P        :
+//            .m._       $$         :
+//           dSMMSSSss.__$b.    __ :
+//          :MMSMMSSSMMMSS$$b  $P ;
+//          SMMMSMMSMMMSSS$$$$     :b
+//         dSMMMSMMMMMMSSMM$$b.dP SSb.
+//        dSMMMMMMMMMMSSMMPT$$=-. /TSSSS.
+//       :SMMMSMMMMMMMSMMP  `b_.'  MMMMSS.
+//       SMMMMMSMMMMMMMMM \  .'\    :SMMMSSS.
+//      dSMSSMMMSMMMMMMMM  \/\_/; .'SSMMMMSSSm
+//     dSMMMMSMMSMMMMMMMM    :.;'" :SSMMMMSSMM;
+//   .MMSSSSSMSSMMMMMMMM;    :.;   MMSMMMMSMMM;
+//  dMSSMMSSSSSSSMMMMMMM;    ;.;   MMMMMMMSMMM
+// :MMMSSSSMMMSSP^TMMMMM     ;.;   MMMMMMMMMMM
+// MMMSMMMMSSSSP   `MMMM     ;.;   :MMMMMMMMM;
+// "TMMMMMMMMMM      TM;    :`.:    MMMMMMMMM;
+//    )MMMMMMM;     _/\    :`.:    :MMMMMMMM
+//   dSS$$MMMb.  |._\\   :`.:     MMMMMMMM
+//   T$S$$$$$$$$$m;O\\"-;`.:_.-  MMMMMMM;
+//  :$$$$$$$$$$$$$$b_l./\ ;`.:    mMMSSMMM;
+//  :$$$$$$$$$$$$$$$$$$$./\;`.:  .$MSMMMMMM
+//   $$$$$$$$$$$$$$$$$$$$. \`.:.$$$SMSSSMMM;
+//   $$$$$$$$$$$$$$$$$$$$$. \.:$$$$SSMMMMMMM
+//   :$$$$$$$$$$$$$$$$$$$$$.//.:$$$SSSSSSSMM;
+//   :$$$$$$$$$$$$$$$$$$$$$$.`.:$SSSSSSSMMMP
+//    $$$$$$$$$;"^J "^$$$$;.`.$P  `SSSMMMM
+//    :$$$$$$$$$       :$$$;.`.P'..   TMMM$b
+//    :$$$$$$$$$;      $$$$;.`/ c^'   d$$$$S;
+//    $$$$S$$$$;      '^^^:_dg:___.$$$$$SSS
+//    $$$SS$$$$;            $$$$$$$$$$$$$SSS;
+//   :$$SSSS$$$$            : $$$$$$$$$$$$SSS
+//   :P"TSSSS$$$            ; $$$$$$$$$$$$SSS;
+//   j    `SSSSS$           :  :$$$$$$$$$$$$SS$
+//  :       "^S^'           :   $$$$$$$$$$$$S$;
+//  ;.____.-;"               "--^$$$$$$$$$$$$P
+//  '-....-"  bug                  ""^^T$$$P"
+
+
+
+
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mk make_pair
+#define ll long long
+#define ss second
+#define ff first
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define mll map<ll,ll>
+#define mod 1000000007
+#define sp " "
+#define w(x) ll x; cin>>x; while(x--)
+#define ps(x,y) fixed<<setprecision(y)<<x;
+#define fo(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define re(i, j) fo(i, 0, j, 1)
+#define pi 3.1415926535897932384626433832795
+#define all(cont) cont.begin(), cont.end()
+#define countbit(x) __builtin_popcount(x)
+#define mod 1000000007
+#define lo lower_bound
+#define de(n) ll n;cin>>n;
+#define def(a,n) ll n;cin>>n;ll a[n];re(i,n){cin>>a[i];}
+#define defi(a,n,k) ll n;cin>>n; ll k;cin>>k;ll a[n];re(i,n){cin>>a[i];}
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define tr(it,a) for(auto it=a.begin();it!=a.end();it++)
+#define nl cout<<endl;
+
+using namespace std;
+//KnightMareVoid
+vector<int> prefix_function(string s){
+    int n=s.length();
+    vector<int> pre(n,0);
+    for(int i=1;i<n;i++){
+        int j=pre[i-1];
+        while(j>0 && s[i]!=s[j]){
+            j=pre[j-1];
+        }
+        if(s[i]==s[j]){
+            j++;
+        }
+        pre[i]=j;
+    }
+    return pre ;
+}
+
+
+int main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        
+
+    }
+
+
+    return 0;
+}
